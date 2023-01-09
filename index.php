558f7bd926e3a3105e56a48e2154ec2bec85928a
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-09 19:14:30 +0100

    aggiunto un construtto e un secondo oggetto

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
     class Movie{
-        public $titolo;
-        public $durata;
+        private $titolo;
+        private $durata;
 
-        function __construct($_durata) {
+        function __construct(int $_durata, string $_titolo ) {
             $this->durata = $_durata ;
+            $this->titolo = $_titolo ;
+
         }
+        
+        function setTitle($_title){
+            $this->titolo = $_title;
+        }
+        function getTitle($_title){
+            return $this->$_title;
+        }
+
     }
 
-$thebatman = new Movie(176);
+$thebatman = new Movie(176, "The Batmana");
+
+
+$bulletTrain= new Movie(127, "Bullet Train");
+
+
+var_dump($thebatman);
